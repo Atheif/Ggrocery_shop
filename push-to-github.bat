a3d2d72f07b66db61f63af1a5638de9fbba287ef
Atheif <sheikathiefathief@gmail.com>
2025-12-15 12:02:35 +0530

    Initial commit: Grocery Shop with Supabase authentication

--- push-to-github.bat
+++ push-to-github.bat
@@ -11,7 +11,7 @@ echo Creating initial commit...
 git commit -m "Initial commit: Grocery Shop with Supabase authentication"
 
 echo Adding remote repository...
-git remote add origin https://github.com/Atheif/grocery-shop.git
+git remote add origin https://github.com/Atheif/Ggrocery_shop.git
 
 echo Setting main branch...
 git branch -M main
@@ -20,6 +20,6 @@ echo Pushing to GitHub...
 git push -u origin main
 
 echo.
-echo Code pushed successfully to https://github.com/Atheif/grocery-shop
+echo Code pushed successfully to https://github.com/Atheif/Ggrocery_shop
 echo.
 pause
